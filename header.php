2787a32a739190e84083392e354c44889dcc0245
Leon Zadravec <lzadravec.mmt-b2011@fh-salzburg.ac.at>
2014-02-06 00:35:36 +0100

    updated content for index. text should be inserted

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title><?php $settings->getTitle(); ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php require_once("includes/style.php"); ?>
 </head>
 <body>
